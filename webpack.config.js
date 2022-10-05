687d4716c412cdd9c6dbc8a382cff155174a1568
Valeria Kharchenko <valerie.kharchenko@gmail.com>
2022-10-05 19:09:45 +0300

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+
+
+const config = {
+    entry: './bomberman/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        fallback: {
+            util: require.resolve("util/"),
+            assert: require.resolve("assert/"),
+            path: require.resolve("path-browserify"),
+            url: require.resolve("url/"),
+            fs: false
+        },
+        alias: {
+            process: "process/browser"
+        }
+    },
+    watch: true,
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            // {
+            //     test: /\.svg$/,
+            //     use: 'file-loader'
+            // },
+            {
+                test: /\.(png|jpg|svg)$/,
+                type: `asset/resource`,
+            },
+            // {
+            //     test: /\.png$/,
+            //     use: [
+            //         {
+            //             loader: 'url-loader',
+            //             options: {
+            //                 mimetype: 'image/png'
+            //             }
+            //         }
+            //     ]
+            // }
+        ]
+    }
+};
+
+module.exports = config;
